f2ae7858db2e3e2d6161de6f76f33e9fb319beca
Pablo Berganza <pberganza10@gmail.com>
2020-11-25 13:18:44 -0600

    :pencil: update docs

--- source/cli.tsx
+++ source/cli.tsx
@@ -10,13 +10,14 @@ Usage
 
 Options
 	--per-page  Results per page
+        --columns  Columns to show (name,price,1h,24h,7d,volume,marketCap)
 
 Usage
   Use the arrow keys to switch page.
   Press 'q' or 'Ctrl+C' to exit.
 
 Examples
-  $ cracket --per-page=1
+  $ cracket --per-page=1 --columns=name,price,1h,24h,7d
 
   Name      Price     1h        24h       7d
   Bitcoin   $18929.9  -0.34865  -1.03432  7.24047
